f70b549499911c26a89a1c308b3f9d1497dfaaa2
Lion-El <68742917+Lion-El@users.noreply.github.com>
2024-01-17 04:31:44 +0000

    initial commit

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+class Modal {
+    constructor(employee) {
+        this.modal = employee;
+        this.card = null;
+    }
+
+    openModalWindow() {
+        this.card = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${this.modal.picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${this.modal.name.first} ${this.modal.name.last}</h3>
+                    <p class="modal-text">${this.modal.email}</p>
+                    <p class="modal-text cap">${this.modal.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${this.modal.phone}</p>
+                    <p class="modal-text">${this.modal.location.street.number} ${this.modal.location.street.name}, 
+                                            ${this.modal.location.state}, ${this.modal.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${this.modal.dob.date}</p>
+                </div>
+            </div>
+        </div>
+        `;
+   }
+}
+
+
+
+
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+const gallery = document.getElementById('gallery');
+const card = document.querySelector('.card');
+let userEmployees;
+
+const modalRepository  = {
+    modalCard: null,
+
+    set newModal(modal) {
+        this.modalCard = modal; 
+    },
+
+    get newModal() {
+        return this.modalCard;
+    }
+}
+
+gallery.addEventListener('click', (e) => {
+    const employeeCard = e.target.closest('.card');
+    const isOutside = !e.target.closest('.card');
+    if (isOutside) {
+        return;
+    } else {
+        employeeCard.classList.remove('remove');
+    }
+
+    const user = userEmployees.results.filter(employee => {
+        return employee.email === employeeCard.querySelector('.card-text').innerText;
+    }).shift();
+    
+    const modal = new Modal(user);
+    modalRepository.newModal = modal;
+    modal.openModalWindow();
+    gallery.insertAdjacentHTML('afterend', modal.card);
+});
+
+document.addEventListener('click', (e) => {
+    const modal = document.querySelector('.modal-container');
+    const closed = e.target.closest('.modal-info-container');
+    if (e.target.closest('.card')) {
+        return
+    } else if (!closed) modal.classList.add('remove');
+});
+
+// collect data and load to page
+async function returnJsonObj(url) {
+    try {
+        const employees = await fetch(url);
+        return await employees.json();
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function displayEmployees() {
+    const employees = await returnJsonObj('https://randomuser.me/api/?results=12');
+    userEmployees = employees;
+    console.log(employees);
+    const employeeCards = employees.results.map(card => {
+        return `<div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="${card.picture.large}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${card.name.first} ${card.name.last}</h3>
+                        <p class="card-text">${card.email}</p>
+                        <p class="card-text cap">${card.location.city}, ${card.location.state}</p>
+                    </div>
+                </div>`
+    });
+    console.log(employeeCards);
+    gallery.insertAdjacentHTML('beforeend', employeeCards);
+}
+
+
+
+displayEmployees();
